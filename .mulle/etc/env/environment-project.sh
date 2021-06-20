6bc66987ffc014d742c96a40d4a13b5815157104
Nat! <nat@mulle-kybernetik.com>
2021-06-20 20:34:41 +0200

    updated mulle-sde

--- .mulle/etc/env/environment-project.sh
+++ .mulle/etc/env/environment-project.sh
@@ -7,19 +7,19 @@ export PROJECT_NAME="mulle-testgen"
 #
 #
 #
-export PROJECT_IDENTIFIER="mulle_testgen"
 
-
-#
-#
-#
-export PROJECT_DOWNCASE_IDENTIFIER="mulle_testgen"
 
 
 #
 #
 #
-export PROJECT_UPCASE_IDENTIFIER="MULLE_TESTGEN"
+
+
+
+#
+#
+#
+
 
 
 #
