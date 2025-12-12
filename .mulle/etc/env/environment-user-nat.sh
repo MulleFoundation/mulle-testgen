27b9adf0022f56c2e45b0739c5ae106e32460686
Nat! <nat@mulle-kybernetik.com>
2025-12-12 14:44:06 +0100

    maintenance: update mulle-sde tooling configuration

--- .mulle/etc/env/environment-user-nat.sh
+++ .mulle/etc/env/environment-user-nat.sh
@@ -4,3 +4,45 @@
 export MULLE_SDE_EDITOR_CHOICE="subl"
 
 
+#
+#
+#
+export MULLE_VIBECODING="YES"
+
+
+#
+#
+#
+export MULLE_SDE_CLEAN_BEFORE_CRAFT="YES"
+
+
+#
+#
+#
+export MULLE_SDE_REFLECT_BEFORE_CRAFT="YES"
+
+
+#
+#
+#
+export MULLE_SDE_CRAFT_BEFORE_RUN="YES"
+
+
+#
+#
+#
+export MULLE_SDE_RUN_TIMEOUT="5"
+
+
+#
+#
+#
+export MULLE_SDE_TEST_AFTER_CRAFT="NO"
+
+
+#
+#
+#
+export MULLE_TEST_CLEAN_BEFORE_RUN="YES"
+
+
